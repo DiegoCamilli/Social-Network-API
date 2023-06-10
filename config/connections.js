d3921cc10554ebc5bdbeaa69c08b86869f2161dc
DiegoCamilli <camillidiego@yahoo.com>
2023-06-09 18:30:04 -0700

    remove sensetive info

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose')
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://DiegoCamilli:Cylonrader99@cool-hw-api.gm4wd40.mongodb.net/'
+  process.env.MONGODB_URI || 'mongodb+srv://DiegoCamilli:passqord@cool-hw-api.gm4wd40.mongodb.net/'
 
 connect(connectionString, {
   useNewUrlParser: true,
