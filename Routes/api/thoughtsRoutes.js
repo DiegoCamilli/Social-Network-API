f5daeec22c93f71ed82790781fff54dffa1b715a
DiegoCamilli <camillidiego@yahoo.com>
2023-06-17 20:59:14 -0700

    fixed routes based on UCLA classword and ASKBCS

--- Routes/api/thoughtsRoutes.js
+++ Routes/api/thoughtsRoutes.js
@@ -1,68 +1,22 @@
-const express = require('express');
-const router = express.Router()
-const Thought = require('../../models')
+const router = require('express').Router()
 
-// Get all thoughts
-router.get('/thoughts', async (req, res) => {
-  try {
-    const thoughts = await Thought.find()
-    res.json(thoughts)
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' })
-  }
-})
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughts')
 
-// Get a single thought by ID
-router.get('/thoughts/:id', async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.id)
-    if (!thought) {
-      return res.status(404).json({ error: 'Thought not found' })
-    }
-    res.json(thought)
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' })
-  }
-})
-
-// Create a new thought
-router.post('/thoughts', async (req, res) => {
-  try {
-    const newThought = await Thought.create(req.body)
-    res.status(201).json(newThought)
-  } catch (error) {
-    res.status(400).json({ error: 'Invalid data' })
-  }
-})
-
-// Update a thought
-router.put('/thoughts/:id', async (req, res) => {
-  try {
-    const updatedThought = await Thought.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    )
-    if (!updatedThought) {
-      return res.status(404).json({ error: 'Thought not found' })
-    }
-    res.json(updatedThought)
-  } catch (error) {
-    res.status(400).json({ error: 'Invalid data' })
-  }
-})
-
-// Delete a thought
-router.delete('/thoughts/:id', async (req, res) => {
-  try {
-    const deletedThought = await Thought.findByIdAndRemove(req.params.id)
-    if (!deletedThought) {
-      return res.status(404).json({ error: 'Thought not found' })
-    }
-    res.json({ message: 'Thought deleted' })
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' })
-  }
-})
+// get thoughts
+router.route('/').get(getAllThoughts).post(createThought)
+// get one thought
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought)
+// get reactions
+router.route('/:id/reactions').post(addReaction)
+// get one reaction
+router.route('/:id/reaction/:reactionId').delete(deleteReaction)
 
 module.exports = router
--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,68 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../../models/User')
+const router = require('express').Router()
 
-// Get all users
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find()
-    res.json(users)
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' })
-  }
-})
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    // addFriend,
+    // deleteFriend
+} = require('../../controllers/users')
 
-// Get a single user by ID
-router.get('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' })
-    }
-    res.json(user)
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' })
-  }
-})
-
-// Create a new user
-router.post('/users', async (req, res) => {
-  try {
-    const newUser = await User.create(req.body)
-    res.status(201).json(newUser)
-  } catch (error) {
-    res.status(400).json({ error: 'Invalid data' })
-  }
-})
-
-// Update a user
-router.put('/users/:id', async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    )
-    if (!updatedUser) {
-      return res.status(404).json({ error: 'User not found' })
-    }
-    res.json(updatedUser)
-  } catch (error) {
-    res.status(400).json({ error: 'Invalid data' })
-  }
-})
-
-// Delete a user
-router.delete('/users/:id', async (req, res) => {
-  try {
-    const deletedUser = await User.findByIdAndRemove(req.params.id)
-    if (!deletedUser) {
-      return res.status(404).json({ error: 'User not found' })
-    }
-    res.json({ message: 'User deleted' })
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' })
-  }
-})
+// get users
+router.route('/').get(getAllUsers).post(createUser)
+// get one user
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser)
+// get friend (bonus)
+// router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend)
 
 module.exports = router
