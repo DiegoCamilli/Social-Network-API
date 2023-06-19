567032d5808a5f076ac7a956fc59a4d638a536ca
DiegoCamilli <camillidiego@yahoo.com>
2023-06-18 18:31:29 -0700

    update data base and connections

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    match: [/.+@.+\..+/, "Must match an email address!"],
   },
   thoughts: [
     {
